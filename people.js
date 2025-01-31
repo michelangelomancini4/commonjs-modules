2071ffa453af351e8b535fae60278fd46e2858e5
michelangelomancini <michelangelomancini8@gmail.com>
2025-01-31 16:52:55 +0100

    consegna punto 3

--- people.js
+++ people.js
@@ -1,11 +1,20 @@
-// 3. Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con due proprietà: fullName ed hobbies. 
-// All’interno della tua funzione, usa le tue due funzioni precedenti per costruire l’oggetto.
-
-
-// importazione file 1: "names.js"
+// importazione funzione file 1: "names.js"
 const nomeCompleto = require("./names");
 
-// importazione file 2: "hobbies.js"
+// importazione funzione file 2: "hobbies.js"
 const hobby = require("./hobbies");
+
+// Crea una funzione che non ha parametri. 
+// La funzione dovrebbe restituire un oggetto con due proprietà: fullName ed hobbies. 
+
+function nameAndHobby() {
+
+    // All’interno della tua funzione, usa le tue due funzioni precedenti per costruire l’oggetto.
+    
+    return {
+        nomeCompleto , hobby
+   };
+    
+}
 
 
